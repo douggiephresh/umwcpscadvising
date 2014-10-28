a1b6f4f96ba40c5af2de542f4e717c69debb268d
douggiephresh <dradoye@mail.umw.edu>
2014-10-28 10:21:45 -0400

    server stuff still

--- server.py
+++ server.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
-import MySQLdb, MySQLdb.cursors 
-import utils
 
 app = Flask(__name__)
 
-
-
-@app.route('/', methods = ['GET','POST'])
+@app.route('/', methods = ["GET", "POST"])
 def login():
   return render_template("login.html")
-
 
 @app.route("/login", methods = ["GET", "POST"])
 def index():
@@ -47,6 +42,15 @@ def index():
               },
             ])
   return render_template("index.html")
+
+@app.route("/process", methods = ["POST","GET"] )
+def processForms():
+  
+  student = request.form['student']
+  studentid = request.form['studentid']
+  year = request.form['year']
+  
+  return (str(request.values.getlist('1checked'))+ str(request.values.getlist('2checked'))+ student + studentid + year)
 
 @app.route('/advise', methods = ['GET','POST'])
 def adviseMain():
@@ -88,16 +92,6 @@ def adviseMain():
     
     return render_template('index.html', class_name = names, class_name2= names, professors = profs)
 
-@app.route("/process", methods = ["POST","GET"] )
-def processForms():
-  
-  student = request.form['student']
-  studentid = request.form['studentid']
-  year = request.form['year']
-  
-  return (str(request.values.getlist('1checked'))+ str(request.values.getlist('2checked'))+ student + studentid + year)
-  
 if __name__ == '__main__':
     app.debug=True
     app.run(host='0.0.0.0', port=3000)
-
